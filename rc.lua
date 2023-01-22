0b7540f7234d9923bd9555010d8496d087a0e574
lucasntakahashi@gmail.com <lucas N. Takahashi>
2023-01-22 05:33:27 -0300

    feat(keybinds): add new keybinds for volume

--- rc.lua
+++ rc.lua
@@ -254,131 +254,148 @@ root.buttons(gears.table.join( awful.button({ }, 3, function () mymainmenu:toggl
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
--- Group awesome
-awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
-{description="show help", group="awesome"}),
-awful.key({ modkey, "Control" }, "s", awesome.restart,
-{description = "reload awesome", group = "awesome"}),
-awful.key({ modkey, "Shift"   }, "s", awesome.quit,
-{description = "quit awesome", group = "awesome"}),
+    -- Group awesome
+    awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
+    {description="show help", group="awesome"}),
+    awful.key({ modkey, "Control" }, "s", awesome.restart,
+    {description = "reload awesome", group = "awesome"}),
+    awful.key({ modkey, "Shift"   }, "s", awesome.quit,
+    {description = "quit awesome", group = "awesome"}),
 
--- Group tag
-awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
-{description = "go back", group = "tag"}),
+    -- Group tag
+    awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
+    {description = "go back", group = "tag"}),
 
--- Group client
-awful.key({ modkey,           }, "j",
-function ()
-    awful.client.focus.byidx( 1)
-end,
-{description = "focus next by index", group = "client"}
-),
-awful.key({ modkey,           }, "k",
-function ()
-    awful.client.focus.byidx(-1)
-end,
-{description = "focus previous by index", group = "client"}
-),
+    -- Group client
+    awful.key({ modkey,           }, "j",
+    function ()
+        awful.client.focus.byidx( 1)
+    end,
+    {description = "focus next by index", group = "client"}
+    ),
+    awful.key({ modkey,           }, "k",
+    function ()
+        awful.client.focus.byidx(-1)
+    end,
+    {description = "focus previous by index", group = "client"}
+    ),
 
-awful.key({ modkey, "Control" }, "n",
-function ()
-    local c = awful.client.restore()
-    -- Focus restored client
-    if c then
-        c:emit_signal(
-        "request::activate", "key.unminimize", {raise = true}
-        )
-    end
-end,
-{description = "restore minimized", group = "client"}),
-
-awful.key({ modkey }, "x",
-function ()
-    awful.prompt.run {
-        prompt       = "Run Lua code: ",
-        textbox      = awful.screen.focused().mypromptbox.widget,
-        exe_callback = awful.util.eval,
-        history_path = awful.util.get_cache_dir() .. "/history_eval"
-    }
-end,
-{description = "lua execute prompt", group = "awesome"}),
-
--- Layout manipulation
-awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
-{description = "swap with next client by index", group = "client"}),
-awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
-{description = "swap with previous client by index", group = "client"}),
-awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
-{description = "focus the next screen", group = "screen"}),
-awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
-{description = "focus the previous screen", group = "screen"}),
-awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
-{description = "jump to urgent client", group = "client"}),
-awful.key({ modkey,           }, "Tab",
-function ()
-    awful.client.focus.history.previous()
-    if client.focus then
-        client.focus:raise()
-    end
-end,
-{description = "go back", group = "client"}),
-
--- Standard program
--- Group launcher
-awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
-{description = "open a terminal", group = "launcher"}),
--- Prompt
-awful.key({ modkey },            "d",     function () awful.screen.focused().mypromptbox:run() end,
-{description = "run prompt", group = "launcher"}),
-
--- Group layout
-awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
-{description = "increase master width factor", group = "layout"}),
-awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
-{description = "decrease master width factor", group = "layout"}),
-awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
-{description = "increase the number of master clients", group = "layout"}),
-awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1, nil, true) end,
-{description = "decrease the number of master clients", group = "layout"}),
-awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1, nil, true)    end,
-{description = "increase the number of columns", group = "layout"}),
-awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
-{description = "decrease the number of columns", group = "layout"}),
-awful.key({ modkey,           }, "space", function () awful.layout.inc( 1)                end,
-{description = "select next", group = "layout"}),
-awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
-{description = "select previous", group = "layout"}),
-
--- Group System
--- Volume Control
-awful.key({}, "XF86AudioRaiseVolume", function ()
-    awful.spawn.easy_async( "wpctl set-volume @DEFAULT_SINK@ 5%+", function ( stdout, stderr, exitreason, exitcode )
-        if (exitcode == 0 ) then
-            awful.spawn.easy_async( "wpctl get-volume @DEFAULT_SINK@" , update_volume )
+    awful.key({ modkey, "Control" }, "n",
+    function ()
+        local c = awful.client.restore()
+        -- Focus restored client
+        if c then
+            c:emit_signal(
+            "request::activate", "key.unminimize", {raise = true}
+            )
         end
-    end)
-end, { description = "raise volume", group = "system"}),
-awful.key({}, "XF86AudioLowerVolume", function ()
-    awful.spawn.easy_async( "wpctl set-volume @DEFAULT_SINK@ 5%-", function ( stdout, stderr, exitreason, exitcode )
-        if (exitcode == 0 ) then
-            awful.spawn.easy_async( "wpctl get-volume @DEFAULT_SINK@" , update_volume )
-        end
-    end)
-end, { description = "lower volume", group = "system"}),
+    end,
+    {description = "restore minimized", group = "client"}),
 
--- Brightness Control
-awful.key({modkey}, "m", function ()
-    awful.spawn.easy_async( "light -A 5", function ( stdout, stderr, exitreason, exitcode )
-        -- if (exitcode == 0 ) then
-        -- end
-    end)
-end, { description = "raise brightness", group = "system"}),
-awful.key({modkey}, "n", function ()
-    awful.spawn.easy_async( "light -U 5", function ( stdout, stderr, exitreason, exitcode )
-        -- if (exitcode == 0 ) then
-        -- end
-    end)
-end, { description = "lower brightness", group = "system"})
+    awful.key({ modkey }, "x",
+    function ()
+        awful.prompt.run {
+            prompt       = "Run Lua code: ",
+            textbox      = awful.screen.focused().mypromptbox.widget,
+            exe_callback = awful.util.eval,
+            history_path = awful.util.get_cache_dir() .. "/history_eval"
+        }
+    end,
+    {description = "lua execute prompt", group = "awesome"}),
+
+    -- Layout manipulation
+    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
+    {description = "swap with next client by index", group = "client"}),
+    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
+    {description = "swap with previous client by index", group = "client"}),
+    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
+    {description = "focus the next screen", group = "screen"}),
+    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
+    {description = "focus the previous screen", group = "screen"}),
+    awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
+    {description = "jump to urgent client", group = "client"}),
+    awful.key({ modkey,           }, "Tab",
+    function ()
+        awful.client.focus.history.previous()
+        if client.focus then
+            client.focus:raise()
+        end
+    end,
+    {description = "go back", group = "client"}),
+
+    -- Standard program
+    -- Group launcher
+    awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
+    {description = "open a terminal", group = "launcher"}),
+    -- Prompt
+    awful.key({ modkey },            "d",     function () awful.screen.focused().mypromptbox:run() end,
+    {description = "run prompt", group = "launcher"}),
+
+    -- Group layout
+    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
+    {description = "increase master width factor", group = "layout"}),
+    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
+    {description = "decrease master width factor", group = "layout"}),
+    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
+    {description = "increase the number of master clients", group = "layout"}),
+    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1, nil, true) end,
+    {description = "decrease the number of master clients", group = "layout"}),
+    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1, nil, true)    end,
+    {description = "increase the number of columns", group = "layout"}),
+    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
+    {description = "decrease the number of columns", group = "layout"}),
+    awful.key({ modkey,           }, "space", function () awful.layout.inc( 1)                end,
+    {description = "select next", group = "layout"}),
+    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
+    {description = "select previous", group = "layout"}),
+
+    -- Group System
+    -- Volume Control
+    awful.key({}, "XF86AudioRaiseVolume", function ()
+        awful.spawn.easy_async( "wpctl set-volume @DEFAULT_SINK@ 5%+", function ( stdout, stderr, exitreason, exitcode )
+            if (exitcode == 0 ) then
+                awful.spawn.easy_async( "wpctl get-volume @DEFAULT_SINK@" , update_volume )
+            end
+        end)
+    end, { description = "raise volume", group = "system"}),
+    awful.key({}, "XF86AudioLowerVolume", function ()
+        awful.spawn.easy_async( "wpctl set-volume @DEFAULT_SINK@ 5%-", function ( stdout, stderr, exitreason, exitcode )
+            if (exitcode == 0 ) then
+                awful.spawn.easy_async( "wpctl get-volume @DEFAULT_SINK@" , update_volume )
+            end
+        end)
+    end, { description = "lower volume", group = "system"}),
+    awful.key({modkey}, "F3", function ()
+        awful.spawn.easy_async( "wpctl set-volume @DEFAULT_SINK@ 5%+", function ( stdout, stderr, exitreason, exitcode )
+            if (exitcode == 0 ) then
+                awful.spawn.easy_async( "wpctl get-volume @DEFAULT_SINK@" , update_volume )
+            end
+        end)
+    end, { description = "raise volume", group = "system"}),
+    awful.key({modkey}, "F2", function ()
+        awful.spawn.easy_async( "wpctl set-volume @DEFAULT_SINK@ 5%-", function ( stdout, stderr, exitreason, exitcode )
+            if (exitcode == 0 ) then
+                awful.spawn.easy_async( "wpctl get-volume @DEFAULT_SINK@" , update_volume )
+            end
+        end)
+    end, { description = "lower volume", group = "system"}),
+
+    -- Brightness Control
+    awful.key({modkey}, "m", function ()
+        awful.spawn.easy_async( "light -A 5", function ( stdout, stderr, exitreason, exitcode )
+            -- if (exitcode == 0 ) then
+            -- end
+        end)
+    end, { description = "raise brightness", group = "system"}),
+    awful.key({modkey}, "n", function ()
+        awful.spawn.easy_async( "light -U 5", function ( stdout, stderr, exitreason, exitcode )
+            -- if (exitcode == 0 ) then
+            -- end
+        end)
+    end, { description = "lower brightness", group = "system"}),
+    awful.key({ }, "XF86AudioPlay" , function ()
+        awful.spawn( "playerctl play-pause" )
+    end, { description = "Play/Pause music", group = "system"})
 )
 
 clientkeys = gears.table.join(
